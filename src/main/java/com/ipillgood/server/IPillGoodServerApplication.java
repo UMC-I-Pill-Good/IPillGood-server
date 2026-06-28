d04a5e8e47abe96525d7cd9274e70a2f54f5f589
zeoueon <zeoueon129@gmail.com>
2026-06-29 01:16:22 +0900

    chore: Spring Boot 프로젝트 초기화

---
+++ src/main/java/com/ipillgood/server/IPillGoodServerApplication.java
@@ -0,0 +1,13 @@
+package com.ipillgood.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPillGoodServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IPillGoodServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipillgood/server/IPillGoodServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ipillgood.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class IPillGoodServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
