0381f7407e337f4b5b3280804e7bdccfe567616d
zeoueon <zeoueon129@gmail.com>
2026-06-30 00:43:06 +0900

    chore: auth 도메인 패키지 컨벤션 틀 추가

---
+++ src/main/java/com/ipillgood/server/domain/auth/controller/AuthController.java
@@ -0,0 +1,6 @@
+package com.ipillgood.server.domain.auth.controller;
+
+import com.ipillgood.server.domain.auth.controller.docs.AuthApi;
+
+public class AuthController implements AuthApi {
+}
---
+++ src/main/java/com/ipillgood/server/domain/auth/dto/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.ipillgood.server.domain.auth.dto;
+
+public class AuthResponse {
+}
