da308277f10689133bbe99ba6fdb12a4f4616926
yeongjun25 <ss91530441@gmail.com>
2026-07-08 17:49:45 +0900

    feat: Member 엔티티 및 Repository 구현

---
+++ src/main/java/com/ipillgood/server/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.ipillgood.server.domain.member.repository;
+
+import com.ipillgood.server.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
