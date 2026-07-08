f52fb7e86038c030c27a4fd5895584789f952116
yeongjun25 <ss91530441@gmail.com>
2026-07-08 11:34:03 +0900

    feat: Auth 도메인 ErrorCode/SuccessCode 구현

--- src/main/java/com/ipillgood/server/domain/auth/code/AuthSuccessCode.java
+++ src/main/java/com/ipillgood/server/domain/auth/code/AuthSuccessCode.java
@@ -9,6 +9,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AuthSuccessCode implements BaseSuccessCode {
 
+    SIGNUP_SUCCESS(HttpStatus.CREATED, "AUTH201_1", "회원가입이 완료되었습니다."),
+    LOGIN_SUCCESS(HttpStatus.OK, "AUTH200_1", "로그인에 성공했습니다."),
+    LOGOUT_SUCCESS(HttpStatus.OK, "AUTH200_2", "로그아웃에 성공했습니다."),
+    TOKEN_REISSUE_SUCCESS(HttpStatus.OK, "AUTH200_3", "토큰 재발급에 성공했습니다."),
     ;
 
     private final HttpStatus status;
