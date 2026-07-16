c8aa85b2ae284bdc8ea7466e33a40eedf89ae0bb
SUNG HYUNJOON <tjdguswns1941@gachon.ac.kr>
2026-07-17 03:32:08 +0900

    feat: enum 빠진 항목 추가

--- src/main/java/com/ipillgood/server/domain/survey/entity/enums/JobType.java
+++ src/main/java/com/ipillgood/server/domain/survey/entity/enums/JobType.java
@@ -12,7 +12,8 @@ public enum JobType {
     PRODUCTION_TECH("생산·기술직"),
     SELF_EMPLOYED("자영업"),
     STUDENT("학생"),
-    HOUSEWIFE("주부");
+    HOUSEWIFE("주부"),
+    ATHLETE_TRAINER("운동선수/트레이너");
 
     private final String label;
 }
