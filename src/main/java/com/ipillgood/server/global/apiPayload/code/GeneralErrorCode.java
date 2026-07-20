a88063875f5b364294ed200d5b70050e84694bb8
zeoueon <zeoueon129@gmail.com>
2026-07-20 23:27:35 +0900

    refactor: auth 도메인과 구분을 위해 응답 코드 수정

--- src/main/java/com/ipillgood/server/global/apiPayload/code/GeneralErrorCode.java
+++ src/main/java/com/ipillgood/server/global/apiPayload/code/GeneralErrorCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum GeneralErrorCode implements BaseErrorCode {
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400_1", "잘못된 요청입니다."),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH401_1", "인증이 필요합니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH403_1", "요청이 거부되었습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401_1", "인증이 필요합니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403_1", "요청이 거부되었습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON404_1", "요청한 리소스를 찾을 수 없습니다"),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON405_1", "지원하지 않는 HTTP 메서드입니다."),
     NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "COMMON406_1", "허용되지 않는 응답 형식입니다."),
