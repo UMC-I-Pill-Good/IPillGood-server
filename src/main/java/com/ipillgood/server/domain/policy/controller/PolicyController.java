598973c9df790e3b19d55e7b7cda9376ba86ba7d
yeongjun25 <ss91530441@gmail.com>
2026-08-09 16:14:58 +0900

    refactor: 약관 목록 조회 API, 상세 조회 API 삭제
    - 약관 목록은 프론트 단에서 고정 데이터로 조회하고 있으므로 API 불필요
    - 상세 조회('>더보기') 역시 프론트 측에서 고정 데이터로 해결. API 불필요

--- src/main/java/com/ipillgood/server/domain/policy/controller/PolicyController.java
+++ src/main/java/com/ipillgood/server/domain/policy/controller/PolicyController.java
@@ -8,7 +8,6 @@ import com.ipillgood.server.domain.policy.service.PolicyService;
 import com.ipillgood.server.global.apiPayload.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,27 +19,11 @@ public class PolicyController implements PolicyApi {
 
     private final PolicyService policyService;
 
-    // 약관/정책 목록 조회
-    @Override
-    @GetMapping
-    public ApiResponse<PolicyResponse.DocumentList> getDocuments() {
-        PolicyResponse.DocumentList response = policyService.getDocuments();
-        return ApiResponse.onSuccess(PolicySuccessCode.POLICY_DOCUMENTS_FOUND, response);
-    }
-
     // 최신 약관/정책 조회
     @Override
     @GetMapping("/latest")
     public ApiResponse<PolicyResponse.LatestDocuments> getLatest(@RequestParam PolicyDocumentType documentType) {
         PolicyResponse.LatestDocuments response = policyService.getLatest(documentType);
         return ApiResponse.onSuccess(PolicySuccessCode.POLICY_LATEST_FOUND, response);
-    }
-
-    // 약관/정책 상세 조회
-    @Override
-    @GetMapping("/{policyDocumentId}")
-    public ApiResponse<PolicyResponse.DocumentDetail> getDocument(@PathVariable Long policyDocumentId) {
-        PolicyResponse.DocumentDetail response = policyService.getDocument(policyDocumentId);
-        return ApiResponse.onSuccess(PolicySuccessCode.POLICY_DOCUMENT_FOUND, response);
     }
 }
